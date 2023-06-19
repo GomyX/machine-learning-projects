9bb18a19daeb61d16558b8c9209a33a3733129b0
Mahmoud Thabet <mahmous010266@gmail.com>
2023-06-19 17:13:00 +0300

    changes

--- implementing svm from scratch/matlab/G_fun.m
+++
@@ -1,7 +0,0 @@
-%Function for the calculation of g(x)
-function [ g ] = G_fun(dataSize, labelTrain, Alpha, b0, K)
-%g(x) calculation formula 
-g = sum(bsxfun(@times, K, (Alpha .* labelTrain)') , 2) + b0 * ...
-                ones(dataSize, 1);
-end
-
